76498c9708187f3dc1db2d4e563157ec434e46d7
Ed <houedward@gmail.com>
2021-03-27 10:16:45 -0700

    added more methods and tests to LinkedList

--- challenges/lib/src/main/java/linkedList/LinkedList.java
+++ challenges/lib/src/main/java/linkedList/LinkedList.java
@@ -1,34 +1,142 @@
 package linkedList;
 
-public class LinkedList<T> {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class LinkedList<T> implements Iterable<T> {
+    @Nullable
     Node<T> head;
-    //Node<T> tail;
+    @Nullable
+    Node<T> tail;
+    private int numElements;
+
+    LinkedList() {
+        numElements = 0;
+    }
 
     public boolean isEmpty() {
         return head == null;
     }
 
     public void insert(T item) {
-        head = new Node(item, this.head);
+        if (isEmpty()) {
+            tail = new Node<>(item, null);
+            head = tail;
+            numElements++;
+            return;
+        }
+        head = new Node<>(item, this.head);
+        numElements++;
     }
 
-    public T pop() throws EmptyListException {
-        if (this.head == null) throw new EmptyListException("Cannot pop empty list");
+    public void insert(Iterable<T> items) {
+        for (T item : items) insert(item);
+    }
+
+    public void append(T item) {
+        if (isEmpty()) {
+            insert(item);
+            return;
+        }
+        assert tail != null;
+        Node<T> newNode = new Node<>(item, null);
+        tail.next = newNode;
+        tail = newNode;
+        numElements++;
+    }
+
+    public void append(Iterable<T> items) {
+        for (T item : items) append(item);
+    }
+
+    public T pop() throws NoSuchElementException {
+        if (isEmpty()) throw new NoSuchElementException("Cannot pop empty list");
+        assert head != null; // Guaranteed by .isEmpty() check;
         T item = head.item;
         head = head.next;
+        numElements--;
+        return item;
+    }
+
+    public T removeFromEnd() throws NoSuchElementException {
+        if (isEmpty()) throw new NoSuchElementException("Cannot pop empty list");
+        assert head != null;
+        Node<T> node = head;
+        if (node.next == null) {
+            T item = node.item;
+            head = null;
+            tail = null;
+            numElements--;
+            return item;
+        }
+        while (node.next.next != null) node = node.next;
+        T item = node.next.item;
+        tail = node;
+        node.next = null;
+        numElements--;
         return item;
     }
 
     public boolean includes(T item) {
-        // return isEmpty() ? false : head.includes(item);
-        if (this.isEmpty()) return false;
-        return head.includes(item);
+        if (isEmpty()) return false;
+        assert head != null;
+        for (Node<T> node = head; node != null; node = node.next) {
+            if (node.item.equals(item)) return true;
+        }
+        return false;
     }
 
     public String toString() {
-        // return head == null ? "NULL" : head.toString();
-        if (head == null) return "NULL";
-        else return head.toString();
+        StringBuilder sb = new StringBuilder();
+        for (Node<T> node = head; node != null; node = node.next)
+            sb.append(String.format("{ %s } -> ", node.item.toString()));
+        sb.append("NULL");
+        return sb.toString();
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        numElements = 0;
+    }
+
+    public int size() {
+        return numElements;
+    }
+
+    public T get(int i) throws IndexOutOfBoundsException {
+        return getNode(i).item;
+    }
+
+    public void set(int i, T item) throws IndexOutOfBoundsException {
+        getNode(i).item = item;
+    }
+
+    private Node<T> getNode(int i) throws IndexOutOfBoundsException {
+        if (i >= this.numElements || i < 0)
+            throw new IndexOutOfBoundsException("List does not have an element " + i);
+        Node<T> node = head;
+        for (int j = 0; j < i; j++) {
+            assert node != null;
+            node = node.next;
+        }
+        return node;
+    }
+
+    @Override @Nonnull
+    public Iterator<T> iterator() {
+        return new LinkedListIterator<>(head);
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        Node<T> node = head;
+        while (node != null) {
+            action.accept(node.item);
+            node = node.next;
+        }
     }
 }
 
@@ -36,25 +144,47 @@ class Node<T> {
     T item;
     Node<T> next;
 
-    public boolean includes (T item) {
-        if (this.item == item) return true;
-        if (this.next == null) return false;
-        return next.includes(item);
-    }
-
     public Node(T item, Node<T> next) {
         this.item = item;
         this.next = next;
     }
 
     public String toString() {
-        if (this.next == null) return String.format ("{ %s } -> NULL", item.toString());
-        else return String.format ("{ %s } -> %s", item.toString(), next.toString());
+        if (next == null) return String.format("{ %s } -> NULL", item.toString());
+        else return String.format("{ %s } -> %s", item.toString(), next.toString());
+    }
+
+    /**
+     * Changes the current next pointer to point to the next of the next. Does not update the size of the LinkedList
+     */
+    public void removeNext() throws NoSuchElementException {
+        if (next == null) throw new NoSuchElementException("Node has no next element");
+        next = next.next;
     }
 }
 
-class EmptyListException extends Exception {
-    public EmptyListException(String message) {
+class LinkedListIterator<T> implements Iterator<T> {
+    Node<T> node;
+
+    public LinkedListIterator(Node<T> node) {
+        this.node = node;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return node != null;
+    }
+
+    @Override
+    public T next() {
+        T item = node.item;
+        node = node.next;
+        return item;
+    }
+}
+
+class NoSuchElementException extends Exception {
+    public NoSuchElementException(String message) {
         super(message);
     }
 }
--- challenges/lib/src/test/java/linkedList/LinkedListTest.java
+++ challenges/lib/src/test/java/linkedList/LinkedListTest.java
@@ -1,8 +1,14 @@
 package linkedList;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.*;
-import static java.util.Optional.ofNullable;
 
 public class LinkedListTest {
     @Test
@@ -12,10 +18,37 @@ public class LinkedListTest {
     }
 
     @Test
-    public void listWithElementsIsNotEmpty () {
+    public void insertMakesListNonEmpty () {
         LinkedList<Integer> l = new LinkedList<>();
         l.insert(5);
         assertFalse("linked list with elements is not empty", l.isEmpty());
+    }
+
+    @Test
+    public void sizeUpdatesOnInsertAndRemove () throws NoSuchElementException {
+        LinkedList<Integer> l = new LinkedList<>();
+        assertEquals("size of empty list", 0, l.size());
+        l.insert(1);
+        assertEquals("size of list after insert", 1, l.size());
+        l.append(2);
+        assertEquals("size of list after append", 2, l.size());
+        l.append(3);
+        l.append(4);
+        assertEquals("size of list after append", 4, l.size());
+        l.insert(5);
+        l.insert(6);
+        assertEquals("size of list after append", 6, l.size());
+        l.removeFromEnd();
+        assertEquals("size of list after removeFromEnd", 5, l.size());
+        l.pop();
+        l.pop();
+        assertEquals("size of list after pop", 3, l.size());
+        l.append(5);
+        l.removeFromEnd();
+        l.removeFromEnd();
+        l.removeFromEnd();
+        l.pop();
+        assertEquals("size of list after inserts and removes", 0, l.size());
     }
 
     @Test
@@ -53,7 +86,7 @@ public class LinkedListTest {
     }
 
     @Test
-    public void toStringOnListWithElements() {
+    public void toStringOnNonEmpty() {
         LinkedList<Integer> l = new LinkedList<>();
         l.insert(3);
         l.insert(2);
@@ -63,9 +96,172 @@ public class LinkedListTest {
     }
 
     @Test
-    public void popOnEmptyThrows() {
-        LinkedList<Integer> l = new LinkedList();
+    public void popOnEmptyThrows() throws NoSuchElementException {
+        LinkedList<Integer> l = new LinkedList<>();
         assertThrows(".pop() on empty list throws",
-                EmptyListException.class, l::pop);
+                NoSuchElementException.class, l::pop);
+        l.insert(5);
+        l.pop();
+        assertThrows(".pop() on empty list throws",
+                NoSuchElementException.class, l::pop);
+    }
+
+    @Test
+    public void popRetrievesElementsLIFO() throws NoSuchElementException {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.insert(3);
+        l.insert(2);
+        l.insert(1);
+        assertEquals(".pop() retrieves elements in last inserted first out order",
+                1, (int) l.pop());
+        assertEquals(".pop() retrieves elements in last inserted first out order",
+                2, (int) l.pop());
+        l.insert(5);
+        l.insert(6);
+        assertEquals(".pop() retrieves elements in last inserted first out order",
+                6, (int) l.pop());
+        assertEquals(".pop() retrieves elements in last inserted first out order",
+                5, (int) l.pop());
+        assertEquals(".pop() retrieves elements in last inserted first out order",
+                3, (int) l.pop());
+    }
+
+    @Test
+    public void removeFromEndOnEmptyThrows() throws NoSuchElementException {
+        LinkedList<Integer> l = new LinkedList<>();
+        assertThrows(".removeFromEnd() on empty list throws",
+                NoSuchElementException.class, l::removeFromEnd);
+        l.insert(1);
+        l.pop();
+        assertThrows(".removeFromEnd() on empty list throws",
+                NoSuchElementException.class, l::removeFromEnd);
+    }
+
+    @Test
+    public void removeFromEndTest1() throws NoSuchElementException {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.insert(3);
+        l.insert(2);
+        l.insert(1);
+        assertEquals(".removeFromEnd() retrieves elements in FIFO order",
+                3, (int) l.removeFromEnd());
+        assertEquals(".removeFromEnd() retrieves elements in FIFO order",
+                2, (int) l.removeFromEnd());
+        l.insert(5);
+        l.insert(6);
+        assertEquals(".removeFromEnd() retrieves elements in FIFO order",
+                1, (int) l.removeFromEnd());
+        assertEquals(".removeFromEnd() retrieves elements in FIFO order",
+                5, (int) l.removeFromEnd());
+        assertEquals(".removeFromEnd() retrieves elements in FIFO order",
+                6, (int) l.removeFromEnd());
+    }
+
+    @Test
+    public void insertIterables() {
+        LinkedList<Integer> l = new LinkedList<>();
+        Iterable<Integer> range = Arrays.asList(1, 2, 3, 4, 5);
+        l.insert(range);
+        for (int i = 5; i > 0; i--) {
+            try {
+                assertEquals("insert on iterable inserts elements in order",
+                        i, (int) l.pop());
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+                Assert.fail();
+            }
+        }
+    }
+
+    @Test
+    public void appendIterables() throws NoSuchElementException {
+        LinkedList<Integer> l = new LinkedList<>();
+        Iterable<Integer> range = Arrays.asList(1, 2, 3, 4, 5);
+        l.append(range);
+        for (int i = 1; i < 6; i++) {
+                assertEquals("append on iterable inserts elements in order",
+                        i, (int) l.pop());
+        }
+    }
+
+    @Test
+    public void iterableInterface() {
+        LinkedList<Integer> l = new LinkedList<>();
+        Integer[] test = {1, 3, 2, 5, 4, 6, 7};
+        List<Integer> range = Arrays.asList(test);
+        for (Integer integer : test) l.append(integer);
+        List<Integer> numbers = new ArrayList<>();
+        int i = 0;
+        for (Integer num : l) {
+            assertEquals("iterable interface traverses list in order",
+                    test[i], num);
+            i++;
+        }
+    }
+
+    @Test
+    public void clearDeletesAllElements() {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.append(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        l.clear();
+        assertTrue("list is empty after clear", l.isEmpty());
+        assertEquals("size of list is 0 after clear", 0, l.size());
+    }
+
+    @Test
+    public void getIndexInBounds() {
+        LinkedList<Integer> l = new LinkedList<>();
+        Integer[] test = {1, 3, 2, 5, 4, 6, 7};
+        l.append(Arrays.asList(test));
+        for (int i = 0; i < test.length; i++) {
+            assertEquals("get finds elements by index starting from 0",
+                    test[i], l.get(i));
+        }
+    }
+
+    @Test
+    public void getIndexOutOfBounds() {
+        LinkedList<Integer> l = new LinkedList<>();
+        Integer[] test = {1, 3, 2, 5, 4, 6, 7};
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.get(-1));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.get(0));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.get(1));
+        l.append(Arrays.asList(test));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.get(-1));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.get(7));
+    }
+
+    @Test
+    public void setIndexInBounds() {
+        LinkedList<Integer> l = new LinkedList<>();
+        Integer[] test = {1, 3, 2, 5, 4, 6, 7};
+        l.append(Arrays.asList(0, 0, 0, 0, 0, 0, 0));
+        for (int i = 0; i < test.length; i++) l.set(i, test[i]);
+        for (int i = 0; i < test.length; i++) {
+            assertEquals(String.format("set() on index %d", i),
+                    test[i], l.get(i));
+        }
+    }
+
+    @Test
+    public void setIndexOutOfBounds() {
+        LinkedList<Integer> l = new LinkedList<>();
+        Integer[] test = {1, 3, 2, 5, 4, 6, 7};
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.set(-1, 0));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.set(0, 0));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.set(1, 0));
+        l.append(Arrays.asList(test));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.set(-1, 0));
+        assertThrows("get with out of bounds index",
+                IndexOutOfBoundsException.class, () -> l.set(7, 0));
     }
 }
